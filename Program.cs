06c6a3975a6045f881808ab05a668bd2e7f07f07
Rhys Wheeler (1000101285) <WHEERH1@student.op.ac.nz>
2025-05-08 11:12:06 +1200

    now work?

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("you are in a room, a snail wants to kill you, try not to die :3");
+            Console.ReadLine();
         }
     }
 }
