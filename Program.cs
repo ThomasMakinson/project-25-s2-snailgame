91314b5adb3ba3eeeb25d48af0983d54506ba3c4
Kavarn Francis <frankm2@student.op.ac.nz>
2025-05-12 11:13:43 +1200

    Working on Title Screen Method

--- Program.cs
+++ Program.cs
@@ -4,9 +4,42 @@ namespace SnailMate
 {
     internal class Program
     {
+        public static void DisplayTitleScreen()
+        { 
+            string[] asciiArt = new string[]
+            {
+                "  ██████  ███▄    █  ▄▄▄       ██▓ ██▓     ███▄ ▄███▓ ▄▄▄     ▄▄▄█████▓▓█████ ",
+                "▒██    ▒  ██ ▀█   █ ▒████▄    ▓██▒▓██▒    ▓██▒▀█▀ ██▒▒████▄   ▓  ██▒ ▓▒▓█   ▀ ",
+                "░ ▓██▄   ▓██  ▀█ ██▒▒██  ▀█▄  ▒██▒▒██░    ▓██    ▓██░▒██  ▀█▄ ▒ ▓██░ ▒░▒███   ",
+                "  ▒   ██▒▓██▒  ▐▌██▒░██▄▄▄▄██ ░██░▒██░    ▒██    ▒██ ░██▄▄▄▄██░ ▓██▓ ░ ▒▓█  ▄ ",
+                "▒██████▒▒▒██░   ▓██░ ▓█   ▓██▒░██░░██████▒▒██▒   ░██▒ ▓█   ▓██▒ ▒██▒ ░ ░▒████▒",
+                "▒ ▒▓▒ ▒ ░░ ▒░   ▒ ▒  ▒▒   ▓▒█░░▓  ░ ▒░▓  ░░ ▒░   ░  ░ ▒▒   ▓▒█░ ▒ ░░   ░░ ▒░ ░",
+                "░ ░▒  ░ ░░ ░░   ░ ▒░  ▒   ▒▒ ░ ▒ ░░ ░ ▒  ░░  ░      ░  ▒   ▒▒ ░   ░     ░ ░  ░",
+                "░  ░  ░     ░   ░ ░   ░   ▒    ▒ ░  ░ ░   ░      ░     ░   ▒    ░         ░   ",
+                "      ░           ░       ░  ░ ░      ░  ░       ░         ░  ░           ░  ░"
+            };
+            int centrex = (Console.WindowWidth - asciiArt.Length / 2), centrey = (Console.WindowHeight / 2);
+
+            Console.Clear();
+            Console.SetCursorPosition(centrex, centrey);
+            Console.WriteLine(asciiArt);
+            Console.WriteLine("1. Start Game");
+            Console.WriteLine("2. Load Previous Game");
+            Console.WriteLine("3. How to Play");
+            Console.WriteLine("4. Start Game");
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
             string direction;
+            DisplayTitleScreen();
             Console.WriteLine("Hello, You are in a room, a snail wants to kill you, good luck :3");
             Console.Write("which direction do you want to go?\nLeft, Right, Up, or Down: ");
             direction = Console.ReadLine().ToLower().Trim();
