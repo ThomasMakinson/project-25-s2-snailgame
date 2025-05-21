05bf17562c54311d787bc51d4dad191773d7b5e3
Rhys Wheeler (1000101285) <WHEERH1@student.op.ac.nz>
2025-05-22 11:10:15 +1200

    added stealth snai; check and commands so you can active checl

--- Program.cs
+++ Program.cs
@@ -65,7 +65,7 @@ namespace SnailMate
                 Console.SetCursorPosition(centredx, verticalStart + i);
                 Console.WriteLine(asciiArt[i]);
             }
-
+            
             Console.ResetColor();
 
             // Display and centre menu options - Kavarn 11:46am
@@ -293,7 +293,7 @@ If a command is not accepted you may have to try other ways of describing your a
                     for (int i = 23; i >= 1; i--)
                     {
                         Console.Clear();
-                        sr = new StreamReader($@"Room-by-Room\2-3\frame ({i}).txt");
+                        sr = new StreamReader($@"Room-by-Room\3-2\frame ({i}).txt");
                         while (!sr.EndOfStream)
                         {
                             aline = sr.ReadLine();
@@ -412,6 +412,8 @@ If a command is not accepted you may have to try other ways of describing your a
                 //need win and death animations
                 //death normal
                 case 1:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    
                     Random random = new Random();
                     int deathSelect;
                     deathSelect = random.Next(61);
@@ -429,6 +431,7 @@ If a command is not accepted you may have to try other ways of describing your a
                             Console.Clear();
                             //runGame = 0;
                             ded = 1;//makes you die
+                            Console.ResetColor();
                             break;
                         case 56:
                             sr = new StreamReader($@"death-screen-stuff\normal\bloodless.txt");
@@ -442,6 +445,7 @@ If a command is not accepted you may have to try other ways of describing your a
                             Console.Clear();
                             //runGame = 0;
                             ded = 1;//makes you die
+                            Console.ResetColor();
                             break;
                         case 57:
                             sr = new StreamReader($@"death-screen-stuff\normal\missionCritical.txt");
@@ -455,6 +459,7 @@ If a command is not accepted you may have to try other ways of describing your a
                             Console.Clear();
                             //runGame = 0;
                             ded = 1;//makes you die
+                            Console.ResetColor();
                             break;
                         case 58:
                             sr = new StreamReader($@"death-screen-stuff\normal\motherF.txt");
@@ -468,6 +473,7 @@ If a command is not accepted you may have to try other ways of describing your a
                             Console.Clear();
                             //runGame = 0;
                             ded = 1;//makes you die
+                            Console.ResetColor();
                             break;
                         case 59:
                             sr = new StreamReader($@"death-screen-stuff\normal\slimey.txt");
@@ -481,6 +487,7 @@ If a command is not accepted you may have to try other ways of describing your a
                             Console.Clear();
                             //runGame = 0;
                             ded = 1;//makes you die
+                            Console.ResetColor();
                             break;
                         case 60:
                             sr = new StreamReader($@"death-screen-stuff\normal\snailTime.txt");
@@ -494,6 +501,7 @@ If a command is not accepted you may have to try other ways of describing your a
                             Console.Clear();
                             //runGame = 0;
                             ded = 1;//makes you die
+                            Console.ResetColor();
                             break;
                         default:
                             sr = new StreamReader($@"death-screen-stuff\normal\youDied.txt");
@@ -507,12 +515,14 @@ If a command is not accepted you may have to try other ways of describing your a
                             Console.Clear();
                             //runGame = 0;
                             ded = 1;//makes you die
+                            Console.ResetColor();
                             break;
 
                     }
                     break;
                  //eldritch snail monster death
                 case 2:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     sr = new StreamReader($@"death-screen-stuff\monster\pt1.txt");
                     while (!sr.EndOfStream)
                     {
@@ -544,8 +554,9 @@ If a command is not accepted you may have to try other ways of describing your a
                     Console.Clear();
                     //runGame = 0;
                     ded = 1;//makes you die
+                    Console.ResetColor();
                     break;
-                    ded = 1;
+                    
                 default:
                     Console.WriteLine("animation not found");
                     break;
@@ -562,30 +573,46 @@ If a command is not accepted you may have to try other ways of describing your a
             snailDistance -= 1;
             if (snailDistance >= 10)
             {
-                text = "The threat is distant.";
+                Console.WriteLine(@"
+The threat is distant.");
             }
             else if (snailDistance >= 5 && snailDistance < 10)
             {
-                text = "The threat draws nearer.";
+                Console.WriteLine(@"
+The threat draws nearer.");
             }
             else if (snailDistance < 5 && snailDistance >1)
             {
-                text = "Breathe softly, it's very close now.";
+                Console.WriteLine(@"
+Breathe softly, it's very close now.");
             }
             else if (snailDistance == 1)
             {
-                text = "It's right behind you.";
+                Console.WriteLine(@"
+It's right behind you.");
             }
             else
             {
-                text = "oh no";
+                Console.WriteLine(@"
+
+oh no");
                 Thread.Sleep(500);
                 int animationID = 1;
                 Animations(ref animationID);
                 ded = 1;
                 
             }
-            Typewriter(text);
+        }
+
+        public static void SnailCheckStealth()
+        {
+            if(snailDistance <=0)
+            {
+                int animationID = 1;
+                Animations(ref animationID);
+                ded = 1;
+            }
+                
         }
         
         public static void Appease()// makes the snail go further away - rhys
@@ -595,11 +622,11 @@ If a command is not accepted you may have to try other ways of describing your a
             {
                 snailDistance += 5;
                 blood -= 1;
-                text = "The sacrifice is accepted.";
+                Console.WriteLine("The sacrifice is accepted.");
             }
             else if (blood == 2)
             {
-                text = "Are you sure? This will be your last: y/n";
+                Console.WriteLine(@"Are you sure? This will be your last: y/n");
                 temp = Console.ReadLine();
                 temp = temp.ToLower().Trim();
 
@@ -607,18 +634,18 @@ If a command is not accepted you may have to try other ways of describing your a
                 {
                     snailDistance += 5;
                     blood -= 1;
-                    text = "The sacrifice is accepted.";
+                    Console.WriteLine("The sacrifice is accepted.");
                 }
                 else
                 {
-                    text = "Very well.";
+                    Console.WriteLine("Very well.");
                 }
             }
             else if (blood == 1)
             {
-                text = "That would kill you. No.";
+                Console.WriteLine("That would kill you. No.");
             }
-            Typewriter(text);
+
         }
 
         public static void DeathCheck(out int runGame)// rhys method to check if ded
@@ -644,7 +671,7 @@ If a command is not accepted you may have to try other ways of describing your a
             Console.Clear();
             while (runGame == 1)// while game is running will loop through whatever room is selected
             {
-                SnailCheck();
+                SnailCheckStealth();
                 DeathCheck(out runGame);
                 switch (roomID)
                 {
@@ -684,13 +711,13 @@ If a command is not accepted you may have to try other ways of describing your a
 
                                         if (hasKey)
                                         {
-                                            text = "You use the Rusty Key to unlock the door.";
+                                            Console.WriteLine("You use the Rusty Key to unlock the door.");
                                             door2lock = 0; // unlocks door
                                             DropFromInventory("Rusty Key"); //remove key after use
                                         }
                                         else
                                         {
-                                            text = "The door is locked. You need a key.";
+                                            Console.WriteLine("The door is locked. You need a key.");
                                             break;
                                         }
                                     }
@@ -728,6 +755,15 @@ If a command is not accepted you may have to try other ways of describing your a
                             case "bleed":
                             case "appease":
                                 Appease();
+                                break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
                                 break;
                             default:
                                 Console.WriteLine("You thought you were smart, huh? What other direction did you think you could go in?");
@@ -767,6 +803,15 @@ But we all that know that that's a stretch.";
                             case "appease":
                                 Appease();
                                 break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
+                                break;
                         }
                         break;
                     case 3:
@@ -801,6 +846,15 @@ But we all that know that that's a stretch.";
                             case "appease":
                                 Appease();
                                 break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
+                                break;
                         }
                         break;
                     case 4:
@@ -828,6 +882,15 @@ But we all that know that that's a stretch.";
                             case "bleed":
                             case "appease":
                                 Appease();
+                                break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
                                 break;
                         }
                         break;
@@ -863,6 +926,15 @@ But we all that know that that's a stretch.";
                             case "appease":
                                 Appease();
                                 break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
+                                break;
                         }
                         break;
                     case 6:
@@ -889,6 +961,15 @@ But we all that know that that's a stretch.";
                             case "bleed":
                             case "appease":
                                 Appease();
+                                break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
                                 break;
                         }
                         break;
@@ -918,6 +999,15 @@ But we all that know that that's a stretch.";
                             case "appease":
                                 Appease();
                                 break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
+                                break;
                         }
                         break;
                     case 8:
@@ -945,6 +1035,15 @@ But we all that know that that's a stretch.";
                             case "bleed":
                             case "appease":
                                 Appease();
+                                break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
                                 break;
                         }
                         break;
@@ -981,6 +1080,15 @@ But we all that know that that's a stretch.";
                             case "appease":
                                 Appease();
                                 break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
+                                break;
                         }
                         break;
                     case 10:
@@ -1009,6 +1117,15 @@ But we all that know that that's a stretch.";
                             case "bleed":
                             case "appease":
                                 Appease();
+                                break;
+                            case "check danger":
+                            case "danger":
+                            case "snail":
+                            case "snailcheck":
+                            case "snail check":
+                            case "how far?":
+                            case "am I going to die?":
+                                SnailCheck();
                                 break;
                         }
                         break;
@@ -1078,7 +1195,7 @@ But we all that know that that's a stretch.";
         {
             
             int userMenuSelection;
-
+            
             do
             {
                 // Displays title screen method then asks for a menu option
